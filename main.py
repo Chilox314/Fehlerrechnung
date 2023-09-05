479b31d517d80e675406905a096520e823debebe
Chilox314 <97847088+Chilox314@users.noreply.github.com>
2023-09-05 13:05:24 +0200

    asdasde

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!\usr\bin\env python3
 import sys
 import numpy as np
+import argparse
 
 def main():
+    args = parsecommandline(sys.argv[1:])
     filename = "test.txt"
     #filename = sys.argv[1]
     try:
@@ -10,30 +12,49 @@ def main():
     except IOError as err:
         sys.stderr.write("{}: {}\n".format(sys.argv[0], err))
         exit(1)
-    
-    l = analyze(stream)
+    if args.statistical_values:
+        
+        l = analyze(stream)
+        save(l)
     stream.close()
-    save(l)
+
+def parsecommandline(argv):
+    p = argparse.ArgumentParser(description="calculate statistical values of some measurements")
+    
+    #p.add_argument('inputfile',type=str,help='inputfile with whitespace or tabseparated values')
+    p.add_argument('-s','--statistical_values',action="store_true")
+    p.add_argument('-l','--linear_regression',action="store_true")
+    p.add_argument('-f','--fehlerfortpflanzungsgesetz',action="store_true")
+
+    return p.parse_args(argv)
 
 def analyze(input_stream):
+    
     l = list()
     for line in input_stream:
         line.rstrip()
         line_list = line.split()
         var = dict()
+
         var["name"] = line_list[0]
         var["values"] = np.array([float(x) for x in line_list[1:]])
         var["n"] = len(var["values"])
+
         print(var)
         var = calculate(var)
+
         print(var) 
         l.append(var)
     return l
 
 def calculate(d):
+
     d["mittelwert"] = np.around(1 / d["n"] * np.sum(d["values"]))
+
     d["standardabweichung_einzel"] = np.around(np.sqrt(1 / (d["n"] - 1) * np.sum((d["values"] - d["mittelwert"])**2)), decimals=4) 
+
     d["standardabweichung_mittel"] = np.around(d["standardabweichung_einzel"] / np.sqrt(d["n"]), decimals=4)
+
     return d
 
 def plot():
@@ -43,14 +64,17 @@ def toLatex():
     return 1
 
 def save(var_list):
+
     try:
         file = open("result.txt", 'a')
     except IOError as err:
         sys.stderr.write("{}: {}\n".format(sys.argv[0], err))
         exit(1)
+
     for elem in var_list:
         file.write("{}: Mittelwert = {}, Standardabweichung(einzeln) = {}, Standardabweichung(Mittelwert) = {}\n"\
                    .format(elem["name"],elem["mittelwert"],elem["standardabweichung_einzel"],elem["standardabweichung_mittel"]))
+        
     file.close()
 
 
---
+++ main2.py
@@ -0,0 +1,37 @@
+#!\usr\bin\env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = input("Was möchtest du berechnen? (s: statistische Werte, l: lineare Regression, f: Fehlerfortpflanzungsgesetz)\n")
+    if x == "s":
+        inputarray = np.array([float(x) for x in input("Gib deine Werte mit Leerzeichen separiert ein:\n").split()])
+        n = len(inputarray)
+        mean = np.mean(inputarray)
+        standard_einzel = np.sqrt(1/(n-1) * np.sum((inputarray - mean)**2))
+        standard_mittel = standard_einzel/ np.sqrt(n)
+        print("Für die Werte {} ergeben sich mit n={}:\n Mittelwert={}\n Standardabweichungen:\n Einzel={}\n Mittel={}\n"\
+              .format(inputarray,n,mean,standard_einzel,standard_mittel))
+    elif x == "l":
+        #x_array = np.array([float(x) for x in input("Gib die x-Werte mit Leerzeichen separiert ein:\n").split()])
+        #y_array = np.array([float(y) for y in input("Gib die y-Werte mit Leerzeichen separiert ein:\n").split()])
+        x_array = np.array([float(x) for x in "8 18 28 38 48".split()])
+        y_array = np.array([float(y) for y in "1000.18 1000.41 1000.64 1000.88 1001.13".split()])
+
+        n = len(x_array)
+
+        x_mean = np.mean(x_array)
+        y_mean = np.mean(y_array)
+
+        b = (np.sum(x_array * y_array) - n*x_mean * y_mean) / (np.sum(x_array ** 2) - n * x_mean**2)
+        a = y_mean - b*x_mean
+
+        d = y_array - b * x_array - a
+
+        s_b = np.sqrt(1/(n-2) * (np.sum(d**2)) / (np.sum((x_array - x_mean) ** 2)))
+        s_a = s_b * np.sqrt(np.sum(x_array ** 2)) / n
+        s_y = np.sqrt(np.sum(d ** 2)) / (n-2)
+        print("x_mean={:.4f}, y_mean={:.4f}, b={:.8f}, a={:.8f}, s_b={:.4e}, s_a={:.4e}".format(x_mean, y_mean, b, a, s_b, s_a))
+
+if __name__ == "__main__":
+    main()
