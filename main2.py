a1c5496129d0ed95cf804650298c9f025283d92e
ChristianMac <syncforcoding@gmail.com>
2023-09-21 16:36:19 +0200

    main2.py V2

--- main2.py
+++ main2.py
@@ -1,11 +1,11 @@
-#!\usr\bin\env python3
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
     x = input("Was möchtest du berechnen? (s: statistische Werte, l: lineare Regression, f: Fehlerfortpflanzungsgesetz)\n")
     if x == "s":
-        inputarray = np.array([float(x) for x in input("Gib deine Werte mit Leerzeichen separiert ein:\n").split(" & ")])
+        inputarray = np.array([float(x) for x in input("Gib deine Werte mit Leerzeichen separiert ein:\n").split()])
         n = len(inputarray)
         mean = np.mean(inputarray)
         standard_einzel = np.sqrt(1/(n-1) * np.sum((inputarray - mean)**2))
@@ -32,7 +32,21 @@ def main():
         s_a = s_b * np.sqrt(np.sum(x_array ** 2)) / n
         s_y = np.sqrt(np.sum(d ** 2)) / (n-2)
         print("x_mean={:.4f}, y_mean={:.4f}, b={:.8f}, a={:.8f}, s_b={:.4e}, s_a={:.4e}".format(x_mean, y_mean, b, a, s_b, s_a))
-        
+        if input("Willst du das Ergebnis plotten?\n") == "y":
+            print(1)
+            fig, ax = plt.subplots(figsize=(10,6.18))
+            ax.set_xlabel(input("X-Achse?\n"))
+            ax.set_ylabel(input("Y-Achse?\n"))
+            ax.set_ylim(0,(1.1) * np.max(y_array))
+            ax.set_xlim(0,1.1 * np.max(x_array))
+            ax.scatter(x_array,y_array,color="red")
+
+            x_max = np.ceil(np.max(x_array))
+            x_list = np.linspace(0, x_max, 5)
+            y_list = b * x_list + a
+
+            ax.plot(x_list,y_list)
+            fig.savefig("figure.pdf")
 
 if __name__ == "__main__":
     main()
